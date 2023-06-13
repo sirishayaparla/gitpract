a35a5e98705c360025fcd8b4061caa7cb01ceaa6
Rupendra <Rupendra@LAPTOP-IUJNIELA>
2023-06-13 12:30:43 +0530

    1st commit

---
+++ src/test/java/Example1.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class Example1 {
+
+
+	@Test
+	public void test1() {
+		System.out.println("Welcome to test yantra");
+	}
+}
---
+++ src/test/java/SR/Exam1.java
@@ -0,0 +1,15 @@
+package SR;
+
+import org.testng.annotations.Test;
+
+public class Exam1 {
+
+	@Test
+	public void test2() {
+		System.out.println("Hellooo");
+		
+		
+		
+		
+	}
+}
